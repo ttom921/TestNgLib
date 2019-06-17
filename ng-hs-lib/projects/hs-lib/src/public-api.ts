e8005a20c7a73aa7e01e6f0778d8418b7145000b
ttom921 <ttom921@hotmail.com>
2019-06-17 15:00:07 +0800

    加入data.service的定義

--- ng-hs-lib/projects/hs-lib/src/public-api.ts
+++ ng-hs-lib/projects/hs-lib/src/public-api.ts
@@ -8,4 +8,5 @@ export * from './lib/hs-lib.service';
 export * from './lib/hs-lib.component';
 export * from './lib/hs-lib.module';
 export * from './lib/hsbutton/hsbutton.component';
+export * from './lib/data.service';
 
